d015b0a469f218704767b87a0c0176c40eac9d5f
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-07-20 21:02:56 +0300

    commit tree b1139aa16aa90e63af7a58179fd69ab0edb363bb

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  /*
+  1.Apply .map to pass each object to a fucntion.
+  2. 
+  
+  */
+   const moviesList = movies.map((element) => {
+     return (
+       <div key={element.title}>
+         <h3>{element.title}</h3>
+         <h4>{element.time}</h4>
+         <ul>
+           {element.genres.map(genre => <li key={genre}>{genre}</li>)}
+         </ul>
+       </div>
+     )
+   })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesList}
+    </div>
+  )
 }
 
 export default Movies;
